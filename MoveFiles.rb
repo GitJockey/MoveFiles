ca2e87de40785db91aedb0237399c0711834729d
GitJockey <GitJockey@users.noreply.github.com>
2014-07-30 15:28:32 -0400

    Refactored for folder usage change

--- MoveFiles.rb
+++ MoveFiles.rb
@@ -1,44 +1,63 @@
-  require 'find'
-  require 'fileutils'
-  require 'yaml'
 
-  ENVIRONMENT = 'development'
-  config=YAML.load_file(ENVIRONMENT+'.yml')
-  
-  Find.find(config['source']) do |f|
-    #loop through, acting on only the jpg and raw files
-    if File.extname(f) == '.jpg' || File.extname(f) == '.rw2'
-      moveFile(f)
-    end
-  end
+class FileMover
+ 	ENVIRONMENT = 'development'
+	def initialize
+		require 'find'
+		require 'fileutils'
+		require 'yaml'
+	  
+		if config=YAML.load_file(ENVIRONMENT+'.yml')
+			puts 'Configuration data loaded:'
+			config.each_with_index {|val, index| puts "	#{val} => #{index}" }
+			puts 
+			if checkDir(config['source'])
+				Find.find(config['source']) do |fileFound|
+					#loop through, acting on only the jpg and raw files
+					if File.extname(fileFound) == '.jpg' || File.extname(fileFound) == '.rw2'
+					  moveFile(fileFound, config['base_dest'])
+					end
+				end
+			else
+				puts 'Source location (' + config['source'] + 'could not be accessed or created.'
+			end
+		else
+			puts 'Unable to load lconfiguration file ' + ENVIRONMENT + '.yml'
+		end
+	end
+	  
+	 def moveFile(myFileName, baseDestination)
+		# make an object based on the path passed, in order to access the file's 
+		#date information    
+		myFile = File.new(myFileName)
+		#check for the folder in the given location with the format YYYY-MM
+		if checkDir(baseDestination + '/' + myFile.ctime.strftime('%Y') + 
+			'/' + myFile.ctime.strftime('%m'))
+		   ##myFile.ctime.strftime('%Y') myFile.ctime.strftime('%m'))
+		  # if folder created or not needed- go ahead and take action
+		  puts 'moving...	FROM: ' + myFileName  
+		  puts ' 		TO: ' + baseDestination + '/' + 
+		   myFile.ctime.strftime('%Y') + '/' + myFile.ctime.strftime('%m') + 
+		   '/' + File.basename(myFileName)
+		  FileUtils.move(myFileName, baseDestination + '/' + 
+		   myFile.ctime.strftime('%Y') + '/' + myFile.ctime.strftime('%m') + 
+		   '/' + File.basename(myFileName))
+		end
+	 end
+	 
+	 def checkDir(myPath) #myYear, myMonth)
+	   #does the folder exist?
+		if Dir.exists?(myPath) 
+			true
+		else 
+			#then make it
+			FileUtils.mkpath(myPath)
+		end
+	 end
 
- def moveFile(myFileName)
-   # make an object based on the path passed, in order to access the file's 
-	#	date information    
-   myFile = File.new(myFileName)
-    #check for the folder in the given location with the format YYYY-MM
-    if checkDir(myFile.ctime.strftime('%Y'), myFile.ctime.strftime('%m'))
-      # if folder created or not needed- go ahead and take action
-      puts 'moving...' + File.name(myFileName) + ' TO: ' + config['base_dest'] + '/' + 
-       myFile.ctime.strftime('%Y') + '/' + myFile.ctime.strftime('%m') + 
-       '/' + File.basename(myFileName)
-      FileUtils.move(myFileName, PIX_BASE_DEST + '/' + 
-       myFile.ctime.strftime('%Y') + '/' + myFile.ctime.strftime('%m') + 
-       '/' + File.basename(myFileName))
-    end
  end
+
+fm = FileMover.new
+
+
  
- def checkDir(myYear, myMonth)
-   #does the year folder not exist?
-    if not Dir.exists?(config['base_dest'] + '/' + myYear) 
-    #then make it
-      Dir.mkdir(config['base_dest'] + '/' + myYear)
-    end
-   #does the month folder not exist?
-    if not Dir.exists?(config['base_dest'] + '/' + myYear + '/' + myMonth) 
-    #then make it
-      Dir.mkdir(config['base_dest'] + '/' + myYear + '/' + myMonth)
-    end
-    true
- end
- 
+
